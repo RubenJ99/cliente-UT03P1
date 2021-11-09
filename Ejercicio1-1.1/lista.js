78e0bcf916d63bbdc3885cf6d75e67d064ad589b
RubenJ99 <juarezperezruben13@gmail.com>
2021-11-09 16:50:43 +0100

    + cositas

--- Ejercicio1-1.1/lista.js
+++ Ejercicio1-1.1/lista.js
@@ -9,6 +9,14 @@ let book = {
   price: 20,
 };
 
+let book2 = {
+  ISBN: 988 - 84 - 9804 - 654 - 0,
+  title: "ElQuijote",
+  author: "MigueldeCervantes",
+  publicationDate: new Date(1605, 0, 1),
+  price: 20,
+};
+
 function create() {
   let list = [];
   return list;
@@ -46,49 +54,77 @@ function toString(list) {
 
   return nList;
 }
+//Tiene que haber una forma de hacerlo asi
+
+// function indexOf(list,elem) {
+// 	let i = 0;
+//   list.forEach((item,index) => {
+// 		if(item.ISBN === elem.ISBN){
+// 			i = index;
+// 		}
+// 	});
+// 	return (i >= 0) ? i : 1;
+// }
 
 function indexOf(list,elem) {
-  
+	let idx = -1;
+	for (let i = 0; i < list.length; i++) {
+		if(elem.ISBN === list[i].ISBN){
+			idx = i;
+			break;
+		}
+	}
+	return (idx >= 0) ? idx : 1;
 }
 
 function lastIndexOf(list,elem) {
-  
+	let idx = -1;
+	for (let i = list.length-1; i > 0; i--) {
+		if(elem.ISBN === list[i].ISBN){
+			idx = i;
+			break;
+		}
+	}
+	return (idx >= 0) ? idx : -1;
 }
 
 function capacity(list) {
-  
+//Preguntar a pablo
 }
 
 function clear(list) {
-  
+	return list = [];
 }
 
 function firstElement(list) {
-  
+	return list[0];
 }
 
 function lastElement(list) {
-  
+	return list[list.length-1];
 }
 
 function remove(list,index) {
-  
+	let a = [];
+	a.splice
 }
 
 function removeElement(list,elem) {
-  
+
 }
 
 function set(list,elem,index) {
-  
+
 }
 
 
 
 const lista = create();
-
 add(lista,book);
 add(lista,book);
 add(lista,book);
-console.log(typeof(toString(lista)));
+add(lista,book2);
+addAt(lista,book2,1);
+console.log(lastIndexOf(lista,book2));
+console.log(indexOf(lista,book2));
 

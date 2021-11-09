acceba0712b80efb2f6be86b5774146b0d9e5dea
RubenJ99 <juarezperezruben13@gmail.com>
2021-11-09 13:29:41 +0100

    Creado index.html y lista.js con 4 funcs

---
+++ Ejercicio1-1.1/lista.js
@@ -0,0 +1,86 @@
+'use strict';
+const limite = 10;
+
+let book = {
+  ISBN: 978 - 84 - 9804 - 654 - 0,
+  title: "ElQuijote",
+  author: "MigueldeCervantes",
+  publicationDate: new Date(1605, 0, 1),
+  price: 20,
+};
+
+function create() {
+  let list = [];
+  return list;
+}
+function isEmpty(list) {
+  return (list.length === 0);
+}
+
+function isFull(list){
+  return (list.length === limite);
+}
+
+function size(list) {
+  return list.length;
+}
+
+function add(list,elem) {
+  list.push(elem);
+  return list.length;
+}
+
+function addAt(list,elem,index) {
+  list.splice(index,0,elem);
+  return list.length;
+}
+
+function get(list,index) {
+  
+}
+
+function toString(list) {
+  
+}
+
+function indexOf(list,elem) {
+  
+}
+
+function lastIndexOf(list,elem) {
+  
+}
+
+function capacity(list) {
+  
+}
+
+function clear(list) {
+  
+}
+
+function firstElement(list) {
+  
+}
+
+function lastElement(list) {
+  
+}
+
+function remove(list,index) {
+  
+}
+
+function removeElement(list,elem) {
+  
+}
+
+function set(list,elem,index) {
+  
+}
+
+
+
+const lista = create();
+console.log(lista);
+
